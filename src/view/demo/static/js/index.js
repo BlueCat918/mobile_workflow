5264a4fa93ee2627d92d1ec1817e471e192700d4
BlueCat <2454157746@qq.com>
2020-06-26 21:04:36 +0800

    :bug: 移除demo中的es6语法

--- src/view/demo/static/js/index.js
+++ src/view/demo/static/js/index.js
@@ -1,25 +1,26 @@
 /*
  * @Author: BlueCat
  * @Date: 2020-06-25 22:50:01
- * @LastEditTime: 2020-06-26 16:27:27
+ * @LastEditTime: 2020-06-26 21:03:39
  * @LastEditors: BlueCat
  * @Description: 
  */
 
 $(function() {
   ajax()
-  setTimeout(() => {
+  setTimeout(function() {
     $('.box').css('background-image', 'linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)')
     test()
   }, 1000)
 
-  const test = () => {
+  var test = function() {
     $('.app').css('background-image', 'linear-gradient(180deg, #2af598 0%, #009efd 100%)')
   }
 
   function ajax() {
     $.get('/v1/getNewUserDiscountTicketDetail?uid=&client_id=&token=&src=web', function(data) {
-      const { serverTimeAt = '' } = data ? data.d : {}
+      var d = data ? data.d : {}
+      var serverTimeAt = d.serverTimeAt || ''
       $('.time').text(`跨域数据：${serverTimeAt}`)
     })
   }
